387ab879fc6edd0db88040ae93ebe922fbadb73b
Shivaansh_Kanchan <kanchangirish@gmail.com>
2020-04-30 15:03:41 +0530

    test

--- sketch.js
+++ sketch.js
@@ -9,9 +9,10 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
+var bg = "sprites/bg.png";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImg();
 }
 
 function setup(){
@@ -45,7 +46,9 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg)
+         background(backgroundImg);
+    
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
@@ -66,7 +69,9 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display();   
+    
+    //getTime();
 }
 
 function mouseDragged(){
@@ -85,4 +90,19 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo"); 
+    var responseJSON = await response.json();
+    //console.log(responseJSON);
+    var datetime = responseJSON.datetime; 
+    var hour = datetime.slice(11,13); 
+    if(hour>=06 && hour<=19){ 
+    bg = "sprites/bg.png"; 
+    } 
+    else{ bg = "sprites/bg2.jpg"; 
+    } 
+    backgroundImg = loadImage(bg); 
+    console.log(backgroundImg); 
 }
